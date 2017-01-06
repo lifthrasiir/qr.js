59c35c79e1a1c7a548a2379666774baf66eb2953
Arthur Speer <ays388@gmail.com>
2017-01-06 19:16:33 +0100

    generatePNG() no longer returns an empty data uri if margin is not set

--- qr.js
+++ qr.js
@@ -770,7 +770,7 @@ var QRCode = {
 		options = options || {};
 		var matrix = QRCode['generate'](data, options);
 		var modsize = Math.max(options.modulesize || 5, 0.5);
-		var margin = Math.max(options.margin !== null ? options.margin : 4, 0.0);
+		var margin = Math.max(options.margin != null ? options.margin : 4, 0.0);
 		var n = matrix.length;
 		var size = modsize * (n + 2 * margin);
 
